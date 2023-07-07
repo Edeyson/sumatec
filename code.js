5e4bf02cb028c3f589bb9520cf184ca1fd43c36b
edeyson <edeyson20@gmail.com>
2023-07-06 22:48:51 -0500

    nuevo registro

--- code.js
+++ code.js
@@ -2,3 +2,13 @@ function agregarProductos(){
     var productos = document.getElementById("productos");
     console.log(productos);
 }
+
+
+const qrcode = new QRCode(document.getElementById('qrcode'), {
+    text: 'https://wa.me/+573164101757',
+    width: 100,
+    height: 100,
+    colorDark : '#000',
+    colorLight : '#fff',
+    correctLevel : QRCode.CorrectLevel.H
+  });
